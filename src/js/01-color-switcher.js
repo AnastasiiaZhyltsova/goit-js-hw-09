4cb4ae3cd8c71e44b9ae08a91f276668047d5bb0
AnastasiiaZhyltsova <anastasiiazhyltsova@gmail.com>
2022-05-27 17:09:02 +0300

    дз_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const refs = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+    // bgColorBody: document.querySelector('body'),
+};
+
+let timerId = null; 
+refs.btnStart.addEventListener('click', onClickStart);
+
+function onClickStart() {
+     refs.btnStart.disabled = true;
+     timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+       
+    }, 1000);
+
+   
+};
+
+refs.btnStop.addEventListener('click', onClickStop);
+
+function onClickStop() {
+    clearInterval(timerId);
+    refs.btnStart.disabled = false;
+   
+};
 
