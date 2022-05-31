123f51898da8b850f9e59055c1139bba91a38860
AnastasiiaZhyltsova <anastasiiazhyltsova@gmail.com>
2022-05-31 13:17:32 +0300

    02-timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -7,67 +7,70 @@ const refs = {
     btnStart: document.querySelector('[data-start]'),
     dataInput: document.querySelector('input#datatime-picker'),
     timer: document.querySelector('.timer'),
-    dataDays: document.querySelector('[data-days]'),
-    dataHours: document.querySelector('[data-hours]'),
-    dataMinutes: document.querySelector('[data-minutes]'),
-    dataSeconds: document.querySelector('[data-seconds]'),
-}
-
+    dateDays: document.querySelector('[data-days]'),
+    dateHours: document.querySelector('[data-hours]'),
+    dateMinutes: document.querySelector('[data-minutes]'),
+    dateSeconds: document.querySelector('[data-seconds]'),
+};
+// вызываем глоюальные переменные 
 let deltaTime = null;
+let currentTime = null;
+let intervalId = null;
+let isActive = false; 
+
 // Библиотека ожидает что её инициализируют на элементе input[type="text"], 
 // поэтому мы добавили в HTML документ поле input#datetime - picker.
-// объект параметров, который является аргументом функции flatpickr(selector, options)
+// объект параметров, который является аргументом функции flatpickr(selector("input#datetime-picker"), options)
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
     onClose(selectedDates) {
-         deltaTime = selectedDates[0] - new Date;
-        console.log(selectedDates[0]);
-        if (selectedDates[0] < new Date) {
+        // выбранная дата в календаре 
+        currentTime = selectedDates[0]; 
+        // отнимаем от выбранной даты текущую дату 
+        deltaTime = currentTime - options.defaultDate;
+        // если выбранная дата меньше текущей, тогда ошибка. кнопка не активна 
+        if (currentTime < options.defaultDate) {
+          refs.btnStart.disabled = true;
           Notiflix.Notify.warning("Please choose a date in the future");
-      }
+        } else {
+            refs.btnStart.disabled = false;
+        }
   },
 };
 flatpickr("input#datetime-picker", options);
+// запуск таймера 
+refs.btnStart.addEventListener('click', onClickStart);
 
-
-class Timer {
-    constructor() {
-        this.intervalId = null;
-        this.isActive = false;  
-    }
-     start() {
-        if (this.isActive) {
+function onClickStart() {
+    // если при запуске таймера он уже активный , то тогда мы выходим из функции, если неактивный, 
+    // тогда делаем его активным, чтобы избежать повторных запусков интервалов бесконечно 
+      if (isActive) {
             return;
         }
-        this.isActive = true;
-        // const startTime = new Date();
-        this.intervalId = setInterval(() => {
-            // const currentTime = Date.now();
-            // deltaTime = currentTime - startTime;
-            const time = convertMs(deltaTime);
-            getTimeComponents(time);
-
-            const delTime = deltaTime - 1000;
-
-            if (delTime <= 0) {
-                clearInterval(this.intervalId);
+    isActive = true;
+    // создаем интервал в котором создем переменную time (она состоит из функции convertMs 
+    // которой предоставляется параметр deltaTime) 
+    intervalId = setInterval(() => {
+        let time = convertMs(deltaTime);
+        getTimeComponents(time);
+                // при каждом запуске intervalId каждую 1 секунду от deltaTime будет отниматься 1сек (обратный счетчик)
+             deltaTime -= 1000;
+                // если deltaTime меньше или = 0 , тогда счетчик очищается 
+            if (deltaTime <= 0) {
+                clearInterval(intervalId);  
             }
-        }, 1000)
-    }
-}  
-const timer = new Timer();
-
-refs.btnStart.addEventListener('click', onClickStart);
-function onClickStart() {
-    timer.start();
+    }, 1000)
+   
 }
-
+//  В интерфейсе таймера необходимо добавлять 0 если в числе меньше двух символов.
 function addLeadingZero(value) {
     return String(value).padStart(2, '0');
 };
+// Для подсчета значений используй готовую функцию convertMs, 
+// где ms - разница между конечной и текущей датой в миллисекундах.
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -86,14 +89,11 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
+// преобразование в текст 
 function getTimeComponents({seconds, minutes, hours, days}) {
-    refs.dataSeconds.textContent = seconds;
-    refs.dataMinutes.textContent = minutes;
-    refs.dataHours.textContent = hours;
-    refs.dataDays.textContent = days;
+    refs.dateSeconds.textContent = seconds;
+    refs.dateMinutes.textContent = minutes;
+    refs.dateHours.textContent = hours;
+    refs.dateDays.textContent = days;
 };
 
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
